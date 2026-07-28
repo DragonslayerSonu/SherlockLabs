47d05bbfd2ae77c1f8ffc1cfcc9911d96aad4206
DragonslayerSonu <sonudasrd999@gmail.com>
2026-07-28 13:23:06 +0000

    Add live metrics and nginx health monitoring

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 from datetime import datetime
 import platform
 import socket
 import psutil
 
 app = Flask(__name__)
+def is_process_running(process_name):
+    for process in psutil.process_iter(["name"]):
+        if process.info["name"] == process_name:
+            return True
 
+    return False
 
 @app.route("/")
 def home():
@@ -16,6 +21,8 @@ def home():
 def dashboard():
     memory = psutil.virtual_memory()
     disk = psutil.disk_usage("/")
+    boot_time = datetime.fromtimestamp(psutil.boot_time())
+    uptime = datetime.now() - boot_time
 
     return render_template(
         "dashboard.html",
@@ -25,9 +32,20 @@ def dashboard():
         python_version=platform.python_version(),
         cpu_usage=psutil.cpu_percent(interval=1),
         ram_usage=memory.percent,
-        disk_usage=disk.percent
+        disk_usage=disk.percent,
+        uptime=str(uptime).split(".")[0],
     )
+@app.route("/api/metrics")
+def metrics():
+    memory = psutil.virtual_memory()
+    disk = psutil.disk_usage("/")
 
+    return jsonify({
+        "cpu": psutil.cpu_percent(),
+        "ram": memory.percent,
+        "disk": disk.percent,
+        "nginx":is_process_running("nginx")
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
